ce1760d1770c72cf9fe89a718520686187875f9e
zainab sbeity <110335052+ZainabSb@users.noreply.github.com>
2024-02-16 20:53:51 +0200

    Update construct_vocab_classification.sh

--- construct_vocab_classification.sh
+++ construct_vocab_classification.sh
@@ -1,5 +1,7 @@
-for dataset in "TREC" "stsa.binary" "stsa.fine" \
-               "custrev" "mpqa" "rt-polarity" "subj"
-do
-    python construct_vocab_classification.py --dataset ${dataset}
-done
+#for dataset in "TREC" "stsa.binary" "stsa.fine" \
+ #              "custrev" "mpqa" "rt-polarity" "subj"
+#do
+ #   python construct_vocab_classification.py --dataset ${dataset}
+#done
+
+python construct_vocab_classification.py --data ${spacy_wikitext-103-raw.train}
